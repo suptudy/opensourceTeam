563daf58e06c9306ed269c046e0a1b5aab799050
JJO <JJO@naver.com>
2022-06-16 13:08:09 +0900

    pants 생성

---
+++ app/src/main/java/open_teamproject/pants.java
@@ -0,0 +1,28 @@
+package open_teamproject;
+
+public class pants implements Clothes{
+    @Override
+    public int gender(){
+        return 0;
+    }; // 남녀공용(0), 여성(1), 남성(2)
+
+    @Override
+    public String product_kind(){ 
+        return "바지";
+    }; // 상품 카테고리
+
+    @Override
+    public String product_name(){
+        return "나이키 조거 팬츠 NSW 클럽 하의 바지 트레이닝복 츄리닝";
+    }; // 상품 이름
+
+    @Override
+    public String brand(){
+        return "나이키";
+    }; // 상품 브랜드
+
+    @Override
+    public float price(){
+        return 34900;
+    }; // 상품 가격
+}
